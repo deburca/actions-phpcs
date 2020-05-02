679a00404f65e33fbe430b3a49e6ef6ca67cdad7
Paddy de Búrca <paddy@deburca.org>
2020-05-02 16:36:04 +0200

    Add possibility to specify extensions to check code style

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,12 @@ else
   DIR_TO_SCAN="$1"
 fi
 
+if [ -z "$2" ]; then
+  EXTENSIONS="php,module,inc,install,test,profile,theme,css,info,txt,md,yml"
+else
+  EXTENSIONS="$2"
+fi
+
 echo "## Running PHPCS on ${DIR_TO_SCAN}"
 echo "PHP Version : ${PHP_FULL_VERSION}"
 
@@ -20,5 +26,5 @@ if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
   exit 2
 fi
 
-echo "php -d memory_limit=-1 /phpcs ${DIR_TO_SCAN} -pv"
+echo "php -d memory_limit=-1 /phpcs ${DIR_TO_SCAN} --extensions=${EXTENSIONS} -pv"
 php -d memory_limit=-1 /phpcs ${DIR_TO_SCAN} -pv
