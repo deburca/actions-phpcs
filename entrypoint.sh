84b25c58f880de830fa1cd9aee4f41066ec66ef5
Paddy de Búrca <paddy@deburca.org>
2020-05-04 21:46:45 +0200

    Enable composer scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,27 +10,9 @@ else
 fi
 
 if [ -z "$2" ]; then
-  EXTENSIONS="php,module,inc,install,test,profile,theme,css,info,txt,md,yml"
+  SCRIPT="php -d memory_limit=-1 /phpcs"
 else
-  EXTENSIONS="$2"
-fi
-
-if [ -z "$3" ]; then
-  STANDARD="DrupalPractice"
-else
-  STANDARD="$3"
-fi
-
-if [ -z "$4" ]; then
-  BIN="/phpcs"
-else
-  BIN="$4"
-fi
-
-if [ -z "$5" ]; then
-  INSTALLED_PATHS="vendor/drupal/coder/coder_sniffer"
-else
-  INSTALLED_PATHS="$5"
+  SCRIPT="composer run-script $2"
 fi
 
 echo "## Running PHPCS on ${DIR_TO_SCAN}"
@@ -44,11 +26,6 @@ if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
   exit 2
 fi
 
-echo "php -d memory_limit=-1 $BIN ${DIR_TO_SCAN} --standard=${STANDARD} --extensions=${EXTENSIONS} -pv"
-echo "setting installed_paths to ${INSTALLED_PATHS}"
+echo "${SCRIPT} ${DIR_TO_SCAN} -pv"
 
-php -d memory_limit=-1 $BIN --config-set installed_paths ${INSTALLED_PATHS}
-
-php -d memory_limit=-1 $BIN -i
-
-php -d memory_limit=-1 $BIN ${DIR_TO_SCAN} --standard=${STANDARD} --extensions=${EXTENSIONS} --ignore="*.min.*" -pv
+${SCRIPT} ${DIR_TO_SCAN} -pv
